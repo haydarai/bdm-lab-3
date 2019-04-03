0cb1a069aff91fb255e2fb6be17a512547d84b95
Haydar Ali Ismail <haydar_ai@yahoo.com>
2019-04-03 20:56:54 +0200

    Finish query 2

--- src/main/java/Q2.java
+++ src/main/java/Q2.java
@@ -1,6 +1,35 @@
-public class Q2 {
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import util.MongoConnection;
 
-	public static void execute() {
+import java.util.Arrays;
 
-	}
+class Q2 {
+
+    static void execute() {
+        MongoConnection connection = new MongoConnection();
+        MongoDatabase database = connection.getDefaultDatabase();
+        MongoCollection<Document> companyCollection = database.getCollection("company");
+
+        AggregateIterable<Document> q2 = companyCollection.aggregate(Arrays.asList(
+                new Document("$lookup", new Document()
+                        .append("from", "person")
+                        .append("localField", "_id")
+                        .append("foreignField", "worksIn")
+                        .append("as", "employees")),
+                new Document("$project", new Document()
+                        .append("_id", true)
+                        .append("employees", new Document("$size", "$employees")))
+        ));
+
+        System.out.println("Q2: For each company, the name and number of employees");
+        for (Document d : q2) {
+            System.out.println(d.getString("_id") + " has " + d.getInteger("employees")
+                    + (d.getInteger("employees") == 1 ? " employee." : " employees."));
+        }
+
+        connection.close();
+    }
 }
