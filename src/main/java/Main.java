771dcaaefd6a22c056ef06938446ad7ad5b91863
Haydar Ali Ismail <haydar_ai@yahoo.com>
2019-04-03 20:59:34 +0200

    Refactor main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,19 @@ public class Main {
 			throw new Exception("Wrong number of parameters, usage: load N (number of documents to create) or Q1/Q2");
 		}
 
-		if (args[0].equals("populate")) {
-			if (Utils.isNumber(args[1])) populateDB.populate(Integer.parseInt(args[1]));
-		}
-		else if (args[0].equals("Q1")) {
-			Q1.execute();
-		}
-		else if (args[0].equals("Q2")) {
-			Q2.execute();
+		switch (args[0]) {
+			case "populate": {
+				if (Utils.isNumber(args[1])) populateDB.populate(Integer.parseInt(args[1]));
+				break;
+			}
+			case "Q1": {
+				Q1.execute();
+				break;
+			}
+			case "Q2": {
+				Q2.execute();
+				break;
+			}
 		}
 	}
 	
